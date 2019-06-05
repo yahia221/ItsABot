5dcd8624e0fc7a03c8ffdd2fbdb8e7f48a4258fc
GotKrypto76 <maxwlang@me.com>
2019-06-04 19:44:55 -0500

    Eslint changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 const logger = require('./modules/logger')();
 const path = require('path');
@@ -11,14 +10,20 @@ client.logger = logger;
 
 client.registry
     .registerGroups([
-        ['fun', 'Fun commands'],
-        ['admin', 'Guild admin commands'],
-        ['botadmin', 'Bot admin commands']
+        [
+            'fun', 'Fun commands',
+        ],
+        [
+            'admin', 'Guild admin commands',
+        ],
+        [
+            'botadmin', 'Bot admin commands',
+        ],
     ])
     .registerDefaults()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-logger.info(`[log][shard ${client.shard.id}] Online!`); // use winston logging, add shard to log message.
+logger.info(`[Shard ${client.shard.id}] Online!`, client.shard.id); // use winston logging, add shard to log message.
 client.login();
 
 
