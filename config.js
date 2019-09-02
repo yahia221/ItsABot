e99b57d2ce2f14d9cebad15b3f59708b10d57bd1
Maxwell <maxwlang@me.com>
2019-09-02 17:50:15 -0500

    Remove deactivated token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.sharding = {};
-config.sharding.token = 'NTgyNzg2Njg4NTc3OTYyMDA2.XOy7NA.aBpS3AsBTyYDoHzQBcx8Nuw3Hqo';
+config.sharding.token = 'token';
 config.sharding.totalShards = 2;
 
 
